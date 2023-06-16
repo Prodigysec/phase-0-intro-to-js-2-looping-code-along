9735b9fe3022b708654cef8c067021126b3a317f
Ian Raburu <williamraburu@gmail.com>
2023-06-16 08:34:17 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+let name = ["Guadalupe", "Ollie", "Aki"];
+
+let occasion = "surprise";
+
+function writeCards(name, occasion) {
+  const thankyouMessage = [];
+  for (let i = 0; i < name.length; i++) {
+    thankyouMessage.push(
+      `Thank you, ${name[i]}, for the wonderful ${occasion} gift!`
+    );
+  }
+  return thankyouMessage;
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
